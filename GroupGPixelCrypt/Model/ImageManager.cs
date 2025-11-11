433e568289eeee0a51b515b686355dd4af1aceec
Cade Allred <callred3@my.westga.edu>
2025-11-11 06:26:03 -0800

    You can now open an image

---
+++ GroupGPixelCrypt/Model/ImageManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace GroupGPixelCrypt.Model
+{
+    public class ImageManager
+    {
+
+        #region Properties
+
+        public SoftwareBitmap SoftwareBitmap { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public static async Task<ImageManager> FromImageFile(StorageFile imageFile)
+        {
+            IRandomAccessStream stream = await imageFile.OpenAsync(FileAccessMode.Read);
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+            return new ImageManager(softwareBitmap);
+        }
+
+        private ImageManager(SoftwareBitmap softwareBitmap)
+        {
+            if (softwareBitmap.BitmapPixelFormat != BitmapPixelFormat.Bgra8 ||
+                softwareBitmap.BitmapAlphaMode == BitmapAlphaMode.Straight)
+            {
+                softwareBitmap = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+            }
+            Debug.WriteLine(softwareBitmap.PixelHeight.ToString() + softwareBitmap.PixelWidth.ToString());
+            this.SoftwareBitmap = softwareBitmap;
+        }
+        
+        #endregion
+    }
+}
---
+++ GroupGPixelCrypt/View/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using GroupGPixelCrypt.Model;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GroupGPixelCrypt
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        #region Data Members
+
+        private ImageManager imageManager;
+
+        #endregion
+
+        #region Constructors
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.openImageButton.Click += this.onOpenImageButtonClicked;
+        }
+
+        #endregion
+
+        #region methods
+
+        private async void onOpenImageButtonClicked(object sender, object eventArgs)
+        {
+            try
+            {
+                FileOpenPicker fileOpenPicker = new FileOpenPicker();
+                fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                fileOpenPicker.FileTypeFilter.Add(".bmp");
+                fileOpenPicker.FileTypeFilter.Add(".png");
+                fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+
+                var inputImage = await fileOpenPicker.PickSingleFileAsync();
+
+                this.imageManager = await ImageManager.FromImageFile(inputImage);
+
+                var source = new SoftwareBitmapSource();
+                await source.SetBitmapAsync(this.imageManager.SoftwareBitmap);
+                this.imageControl.Source = source;
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message);
+            }
+        }
+
+        #endregion
+    }
+}
